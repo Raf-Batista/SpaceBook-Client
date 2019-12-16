daa2b75b5a00fe84c9ee60f11b6a4b39c6c0d4b3
Rafael <RafaelB24@yahoo.com>
2019-12-15 21:14:28 -0800

    Create components/Navbar.js, import Navbar in App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Home/>
     </div>
   );
