edf9a46f3677716e427ffce09b269057097ebdcf
Rafael <RafaelB24@yahoo.com>
2020-01-29 06:07:42 -0800

    Add responsive breakpoints to App.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,17 @@ import Login from './Login';
 class Navbar extends Component {
     render() {
         return(
-            <div className="navBarStyle">
-                <nav className="navbar ">
-                {/* <span style={{fontSize: '3em', color: 'White'}}> // Rocketship Logo from Font Awesome
-                    <i class="fas fa-space-shuttle"></i>
-                </span> */}
-                    
-                    <a className="homeButton" href="#">SpaceBook</a>  
-                    <Login/>
-                </nav>
+            <div className="Navbar">
+                    <div className="navBarStyle">
+                        <nav className="navbar ">
+                        {/* <span style={{fontSize: '3em', color: 'White'}}> // Rocketship Logo from Font Awesome
+                            <i class="fas fa-space-shuttle"></i>
+                        </span> */}
+                            
+                            <a className="navbar-brand" href="#">SpaceBook</a>  
+                            <Login/>
+                        </nav>
+                    </div>
             </div>
         )
     }
